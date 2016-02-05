7503aff08ce97e5672fb0edee87ab0636d6f7e7e
icecoolcat <icecoolcat@users.noreply.github.com>
2016-02-05 10:28:11 +0800

    implement the grid view

--- app/src/main/java/com/example/android/mdpandroid/MainActivity.java
+++ app/src/main/java/com/example/android/mdpandroid/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // -------------------------------Robot & Maze-------------------------
+        gridView = (GridView) findViewById(R.id.gridView1);
+        robot[0] = 0;
+        robot[1] = 1;
+        robot[2] = 2;
+        robot[3] = 20;
+        robot[4] = 21;
+        robot[5] = 22;
+        robot[6] = 40;
+        robot[7] = 41;
+        robot[8] = 42;
+        int x = 0;
+        for(int i=0; i<arena_maze.length; i++) {
+            if(x<9 && i==robot[x]) {
+                if( x != 5)
+                    arena_maze[i] = 1;
+                else
+                    arena_maze[i] = 2;
+                x++;
+            }else {
+                arena_maze[i] = 0;
+            }
+        }
+        gridView.setAdapter(new Maze(this, arena_maze));
     }
 
     @Override
@@ -35,7 +61,16 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+
+    //Grid initialisation
+    GridView gridView;
+    int robot [] = new int[9];
+    static final int[] arena_maze = new int [300];
+    String robotpos = "UP";
+
+
+
 }
---
+++ app/src/main/java/com/example/android/mdpandroid/Maze.java
@@ -0,0 +1,83 @@
+package com.example.android.mdpandroid;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by SaiMunLee on 5/2/16.
+ */
+public class Maze extends BaseAdapter {
+    private Context context;
+    private final int[] mazeValues;
+
+    //Robot robot = setupRobot();
+
+    public Maze(Context context, int[] mazeValues) {
+        this.context = context;
+        this.mazeValues = mazeValues;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+
+            gridView = new View(context);
+
+            // get layout from maze.xml
+            gridView = inflater.inflate(R.layout.maze, null);
+
+            // set value into textview
+            TextView textView = (TextView) gridView
+                    .findViewById(R.id.grid_item_label);
+
+            //textView.setText("");
+            textView.setText(Integer.toString(position));
+
+            int grid = mazeValues[position];
+
+            if (grid==0) {
+                textView.setBackgroundColor(Color.DKGRAY);
+            } else if (grid==1) {
+                textView.setBackgroundColor(Color.parseColor("#FE9A2E"));
+            } else if (grid==2) {
+                textView.setBackgroundColor(Color.WHITE);
+            } else {
+
+            }
+
+            //int mobile = mobileValues[position];
+
+        } else {
+            gridView = (View) convertView;
+        }
+
+        return gridView;
+    }
+
+    @Override
+    public int getCount() {
+        return mazeValues.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+}
+
+
